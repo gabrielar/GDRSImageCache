88e68ad7c34283979dc8966c3c24172adce4fbd8
Gabriel Radu <gabriel.adrian.radu@gmail.com>
2014-05-04 22:36:17 +0100

    Factor out resizing code

--- GDRSImageCache/UIImage+GDRSResizing.m
+++ GDRSImageCache/UIImage+GDRSResizing.m
@@ -8,31 +8,53 @@
 
 #import "UIImage+GDRSResizing.h"
 
+typedef CGSize(^UIImageGDRSResizingScalingFactorComputation)(CGSize sizeRatio);
+typedef UIBezierPath*(^UIImageGDRSResizingClippingPathComputation)(CGRect newImageRect, CGRect targetRect);
+
+
 @implementation UIImage (GDRSResizing)
 
 - (UIImage *)gadrs_resizedImageToFitSize:(CGSize)newImageSize cornerRadius:(CGFloat)cornerRadius
 {
+    return [self gadrs_resizedImageToFitSize:newImageSize scaleingComputation:^CGSize(CGSize sizeRatio) {
+        
+        CGFloat scalingFactor = MIN(sizeRatio.width, sizeRatio.height);
+        return CGSizeMake(scalingFactor, scalingFactor);
+        
+    } clippingComputation:^UIBezierPath *(CGRect newImageRect, CGRect targetRect) {
+        
+         return [UIBezierPath bezierPathWithRoundedRect:targetRect cornerRadius:cornerRadius];
+        
+    }];
+}
+
+- (UIImage *)gadrs_resizedImageToFitSize:(CGSize)newImageSize
+                     scaleingComputation:(UIImageGDRSResizingScalingFactorComputation)scalingComputation
+                     clippingComputation:(UIImageGDRSResizingClippingPathComputation)clippingComputation
+{
     UIImage *image = nil;
     
     CGFloat resizedImageScale = [[UIScreen mainScreen] scale];
-//    CGFloat selfScale = self.scale;
+    //    CGFloat selfScale = self.scale;
     
     CGSize sizeRatio = CGSizeMake(newImageSize.width/self.size.width,
                                   newImageSize.height/self.size.height);
     
-    CGFloat scalingFactor = MIN(sizeRatio.width, sizeRatio.height);
+    CGSize scalingFactor = scalingComputation(sizeRatio);
     
     CGRect targetRect = CGRectZero;
-    targetRect.size.width = self.size.width * scalingFactor;
-    targetRect.size.height = self.size.height *scalingFactor;
+    targetRect.size.width = self.size.width * scalingFactor.width;
+    targetRect.size.height = self.size.height *scalingFactor.height;
     targetRect.origin.x = (newImageSize.width - targetRect.size.width) / 2.0f;
     targetRect.origin.y = (newImageSize.height - targetRect.size.height) / 2.0f;
     
     UIGraphicsBeginImageContextWithOptions(targetRect.size, NO, resizedImageScale); {
         
-        UIBezierPath *clippingPath = [UIBezierPath bezierPathWithRoundedRect:targetRect cornerRadius:cornerRadius];
-        //[clippingPath stroke];
-        [clippingPath addClip];
+        if (clippingComputation) {
+            CGRect newImageRect =  { CGPointZero, newImageSize };
+            UIBezierPath *clippingPath = clippingComputation(newImageRect, targetRect);
+            [clippingPath addClip];
+        }
         
         [self drawInRect:targetRect];
         image = UIGraphicsGetImageFromCurrentImageContext();
@@ -41,6 +63,5 @@
     
     return image;
 }
-
 
 @end
