8ad17fc170720f36405c6697b820cb0c8b8be8e3
Gabriel Radu <gabriel.adrian.radu@gmail.com>
2014-05-12 14:15:27 +0100

    Add headerdoc comments

--- GDRSImageCache/GDRSImageCache.h
+++ GDRSImageCache/GDRSImageCache.h
@@ -9,23 +9,97 @@
 #import "UIImage+GDRSResizing.h"
 
 
-
+/**
+ *  Callback which transforms an image to another image. It is normally
+ *  used to filter the image fetched over the network into one that is
+ *  cached.
+ *
+ *  @param sourceImage The source image.
+ *
+ *  @return The filtered (transformed) image.
+ */
 typedef UIImage*(^GDRSImageCacheImageFilter)(UIImage *sourceImage);
 
-
-
+/**
+ *  Callback called when an image has been fetched.
+ *
+ *  @param image The image fetched and some time processed (e.g.
+ *               through a filter).
+ *  @param error Points to an error if one has occurred during fetching
+ *               otherwise nil.
+ */
 typedef void(^GDRSImageCacheFetchCompletionHandler)(UIImage *image, NSError *error);
 
 
 
+/**
+ *  Memory based cache for UIImage objects.
+ * 
+ *  A typical usage example of this class is:
+ *  @code
+         UIImageView *anImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+ 
+         // create the cache
+         GDRSImageCache *cache = [[GDRSImageCache alloc] initWithCachedImageFilter:^UIImage *(UIImage *sourceImage) {
+         return [sourceImage gadrs_resizedImageToAspectFitSize:anImageView.bounds.size cornerRadius:10];
+         }];
+ 
+         // set the default image, which will be returned from
+         // fetchImageWithURL:completionHandler: if an image coresponding to the
+         // requested url is not cached yet.
+         cache.defaultImage = [UIImage imageNamed:<#place holder image name#>];
+ 
+ 
+         NSURL *imageUrl = <#an url to a image#>;
+ 
+         // fetch an image; the call returns imidiatly and the callback handler
+         // is called when the image is fetched over the network
+         anImageView.image = [cache fetchImageWithURL:imageUrl completionHandler:^(UIImage *image, NSError *error) {
+         anImageView.image = image;
+         }];
+ *  @endcode
+ */
 @interface GDRSImageCache : NSObject
 
+/**
+ *  Temporary image which is returned for convenience when the requested
+ *  image is not found in the cache. This image can be used until the
+ *  requested image is fetched over the network.
+ */
 @property (nonatomic) UIImage *defaultImage;
 
+/**
+ *  Filter applied strait after the image has been fetch. Usually such a 
+ *  filter is used to resize the image in order to fit the a certain
+ *  UIImageView. This filter is called on a background thread before the 
+ *  image is cached.
+ */
 @property (nonatomic, readonly, copy) GDRSImageCacheImageFilter imageFilter;
 
+/**
+ *  Initializes the cache with an image filter.
+ *
+ *  @param imageFilter Filter applied strait after the image has been fetch. 
+ *                     Usually such a filter is used to resize the image in 
+ *                     order to fit the a certain UIImageView. This filter is 
+ *                     called on a background thread before the image is cached.
+ *
+ *  @return The initialized instance.
+ */
 - (instancetype)initWithCachedImageFilter:(GDRSImageCacheImageFilter)imageFilter;
 
+/**
+ *  Initiates an image fetch over the network and returns the cached image if
+ *  one is already cached, or the defaultImage if a cached image is not found.
+ *
+ *  @param imageURL          The url where the image must be fetched from.
+ *  @param completionHandler Called when the image has been fetched. It is not 
+ *                           called back if the image is not fetched from over 
+ *                           the network for some reason.
+ *
+ *  @return The cached image or the defaultImage if the image has not been 
+ *          cached yet.
+ */
 - (UIImage *)fetchImageWithURL:(NSURL *)imageURL completionHandler:(GDRSImageCacheFetchCompletionHandler)completionHandler;
 
 @end
--- GDRSImageCache/UIImage+GDRSResizing.h
+++ GDRSImageCache/UIImage+GDRSResizing.h
@@ -8,18 +8,109 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  Callback handler which must compute and return a bezier path. This
+ *  path will be applied as clipping mask for the new image.
+ *
+ *  @param newImageRect The bounds of the new (resized) image.
+ *  @param targetRect   The rect where the old image will be drawn in
+ *  the new image.
+ *
+ *  @return This bezier path will be applied as clipping mask for the 
+ *  new image.
+ */
 typedef UIBezierPath*(^UIImageGDRSResizingClippingPathComputation)(CGRect newImageRect, CGRect targetRect);
 
+
+/**
+ *  Adds methods for resizing the receiver.
+ */
 @interface UIImage (GDRSResizing)
 
+
+/// @name Aspect fit methods
+
+/**
+ *  Returns a scaled image of the receiver which which fits the provided 
+ *  size while maintaining the aspect ratio. Any remaining area of the 
+ *  new image bounds is transparent. Can be called on a background thread.
+ *
+ *  @param newImageSize The size of the new (resized) image.
+ *
+ *  @return A image with the size of newImageSize
+ */
 - (UIImage *)gadrs_resizedImageToAspectFitSize:(CGSize)newImageSize;
+
+/**
+ *  Returns a scaled image of the receiver which which fits the provided
+ *  size while maintaining the aspect ratio. Any remaining area of the
+ *  new image bounds is transparent. Can be called on a background thread.
+ *
+ *  @param newImageSize The size of the new (resized) image.
+ *  @param cornerRadius The radius to use in drawing the rounded corners 
+ *                      the new image. Note that the rounded corner are 
+ *                      drawn around the actual image, not around any
+ *                      remaining transparent area.
+ *
+ *  @return A image with the size of newImageSize
+ */
 - (UIImage *)gadrs_resizedImageToAspectFitSize:(CGSize)newImageSize cornerRadius:(CGFloat)cornerRadius;
+
+/**
+ *  Returns a scaled image of the receiver which which fits the provided
+ *  size while maintaining the aspect ratio. Any remaining area of the
+ *  new image bounds is transparent. Can be called on a background thread.
+ *
+ *  @param newImageSize The size of the new (resized) image.
+ *  @param clippingComputation Callback handler which must compute and
+ *                             return a bezier path. This path will be 
+ *                             applied as clipping mask for the new image.
+ *
+ *  @return A image with the size of newImageSize
+ */
 - (UIImage *)gadrs_resizedImageToAspectFitSize:(CGSize)newImageSize
                            clippingComputation:(UIImageGDRSResizingClippingPathComputation)clippingComputation;
 
 
+
+/// @name Aspect fill methods
+
+/**
+ *  Returns a scaled version of the receiver which fills the provided size 
+ *  while retaining the aspect ratio of the image. Some portion of the content 
+ *  may be clipped to fill the new size. Can be called on a background thread.
+ *
+ *  @param newImageSize The size of the new (resized) image.
+ *
+ *  @return A image with the size of newImageSize
+ */
 - (UIImage *)gadrs_resizedImageToAspectFillSize:(CGSize)newImageSize;
+
+/**
+ *  Returns a scaled version of the receiver which fills the provided size
+ *  while retaining the aspect ratio of the image. Some portion of the content
+ *  may be clipped to fill the new size. Can be called on a background thread.
+ *
+ *  @param newImageSize The size of the new (resized) image.
+ *  @param cornerRadius The radius to use in drawing the rounded corners
+ *                      the new image.
+ *
+ *  @return A image with the size of newImageSize
+ */
 - (UIImage *)gadrs_resizedImageToAspectFillSize:(CGSize)newImageSize cornerRadius:(CGFloat)cornerRadius;
+
+/**
+ *  Returns a scaled version of the receiver which fills the provided size
+ *  while retaining the aspect ratio of the image. Some portion of the content
+ *  may be clipped to fill the new size. Can be called on a background thread.
+ *
+ *  @param newImageSize The size of the new (resized) image.
+ *  @param clippingComputation Callback handler which must compute and
+ *                             return a bezier path. This path will be
+ *                             applied as clipping mask for the new image.
+ *
+ *  @return A image with the size of newImageSize
+ */
 - (UIImage *)gadrs_resizedImageToAspectFillSize:(CGSize)newImageSize
                             clippingComputation:(UIImageGDRSResizingClippingPathComputation)clippingComputation;
 
